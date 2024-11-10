1fbb7ec0b7ddfc49f15d6fdd1fb89b181cccc47c
Miroslav Tsarev <mtzarev@gmail.com>
2024-11-10 23:27:55 +0200

    Update SumNumbers.js

--- SumNumbers.js
+++ SumNumbers.js
@@ -4,13 +4,13 @@ function calc() {
    let num2 = document.getElementById(`num2`).value;
    let sum = 0
    if (symbol === "+"){
-   sum = Number(num1)+Number(num2)
+   sum = Number(num1)+Number(num2);
    }else if (symbol === "*"){
-    sum = Number(num1)*Number(num2)
+    sum = Number(num1)*Number(num2);
    }else if (symbol === "/"){
-    sum = Number(num1)/Number(num2)
+    sum = Number(num1)/Number(num2);
    }else if (symbol === "%"){
-    sum = Number(((num1/num2)*100))
+    sum = Number(((num1/num2)*100));
    }
-   document.getElementById(`sum`).value = sum
+   document.getElementById(`sum`).value = sum;
 }
